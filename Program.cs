464de707c6101a34c1e064cab03f3ad975677a85
LOLA-BANKS <you@example.com>
2022-07-25 16:21:54 +0100

    initial committ

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace GITProject
         {
             Console.WriteLine("This is my first GIT Project");
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
